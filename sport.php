b4f5f51083b6989784250b01ed43818ed4a6df0f
michelepor <porceddu.michele@gmail.com>
2018-02-01 14:47:58 +0000

    removed duplicates from list

--- sport.php
+++ sport.php
@@ -27,6 +27,7 @@ include("auth.php");
       $team = stripslashes($_POST['team']);
       $csv = file_get_contents('http://www.football-data.co.uk/mmz4281/1718/I1.csv');
       $rows = explode("\n",$csv);
+      $losers = array();
       echo "Your team won against:<br>";
       foreach($rows as $row) {
         //Div	Date	HomeTeam	AwayTeam	FTHG	FTAG	FTR
@@ -34,11 +35,15 @@ include("auth.php");
         //Skip the match if here was a draw
         if ($result!="D") {
           // if the chosen team was the winning home team, echo loser away team
-          if ((strcasecmp($team, $home) == 0) && ($result=="H")) echo $away."<br>";
+          if ((strcasecmp($team, $home) == 0) && ($result=="H")) $losers[] = $away;
           // if the chosen team was the winning away team, echo loser home team
-          if ((strcasecmp($team, $away) == 0) && ($result=="A")) echo $home."<br>";
+          if ((strcasecmp($team, $away) == 0) && ($result=="A")) $losers[] = $home;
         }
-      }  
+      }
+      $losers = array_unique($losers);
+      sort($losers);
+      foreach ($losers as $loser) echo $loser."<br>";
+
     }
     ?>
 
